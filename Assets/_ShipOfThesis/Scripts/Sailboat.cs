380e3055b6184a701df79c94a3ff947bd8ce64f6
stevekrueger52@gmail.com <stevekrueger52@gmail.com>
2022-03-07 18:12:30 -0500

    no message

--- Assets/_ShipOfThesis/Scripts/Sailboat.cs
+++ Assets/_ShipOfThesis/Scripts/Sailboat.cs
@@ -1,22 +1,33 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Cinemachine.Utility;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Sailboat : MonoBehaviour , WindZone.IWindObject
 {
     #region Public Members
-
-    public float maxSpeed;
     
+    public bool SimpleControls { get; private set; }
+  
+    [Header("Physics Constraints")]
+    public float maxSpeed;
+
+    public float maxSailAngleSpeed;
+    public float SailOuterEaseThreshold;
+    public float SailInnerEaseThreshold;
+
+    [Header("Sailing Curves")]
     // All Animation Curves are in the range of X,Y = [0,1].
     // For angles, this translates to 0 = 0 and 180 = 1 (wind) and 90 = 1 (sail), with assumed symmetry for negative angles.
     // Furthermore, there will be exactly 1 X value for every Y value and vice versa,
     // even if this cannot be enforced in code.
-    
+
     // The three graphed lines will approximate this chart of projected speeds by boat heading
     // https://physicstoday.scitation.org/na101/home/literatum/publisher/aip/journals/content/pto/2008/pto.2008.61.issue-2/1.2883908/production/images/medium/1.2883908.figures.f5.gif
     // The Physics of Sailing, Physics Today 61, 2, 38 (2008); https://doi.org/10.1063/1.2883908
-    public AnimationCurve Knots5, Knots10, Knots20;
+    public AnimationCurve Knots5;
+    public AnimationCurve Knots10, Knots20;
     [Space] 
     
     // Used to approximate the best sail angle at a given point of sail, for wind effect drop off
@@ -30,14 +41,21 @@ public class Sailboat : MonoBehaviour , WindZone.IWindObject
     // Assume the angle of attack (between apparent wind and sail) is optimal.
     // Find the optimal boat heading for that angle of attack (inverse of OptimalSailAngle)
     // Determine the boat's optimal speed for the faux heading, and apply falloff based on the angle difference.
+
+    
     
     
     #endregion
 
     #region Private Members
 
-    private float mpsToKnots = 1.94384f;
+    private float mpsToKnots = 1.94384f; // Meters per second to knots
     private List<WindZone> windZones;
+    private Rigidbody rb;
+
+    private float sailHeight;
+    private float sailAngle;
+    private float rudderAngle;
 
     #endregion
 
@@ -45,11 +63,26 @@ public class Sailboat : MonoBehaviour , WindZone.IWindObject
     
     public void EnterWind(WindZone wind) { windZones.Add(wind); }
     public void ExitWind(WindZone wind) { windZones.Remove(wind); }
-    
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     private void FixedUpdate()
     {
-        Vector2 wind = AverageWind().ProjectOntoPlane(Vector3.up);
+        // Determine Constants for frame
+        Vector2 flatVelocity = rb.velocity.ProjectOntoPlane(Vector3.up);
+        Vector2 trueWind = AverageWind().ProjectOntoPlane(Vector3.up);
+        Vector2 apparentWind = trueWind - flatVelocity;
         Vector2 header = transform.forward.ProjectOntoPlane(Vector3.up);
+
+        float windAngle = Vector2.SignedAngle(-header, apparentWind);
+
+        // Seek Sail to Apparent Wind Heading
+        float deltaAngle = sailAngle - windAngle;
+        float newAngle = SeekEase(sailAngle, windAngle, maxSailAngleSpeed, SailOuterEaseThreshold,
+            SailInnerEaseThreshold);
     }
     
     #endregion
@@ -70,7 +103,16 @@ public class Sailboat : MonoBehaviour , WindZone.IWindObject
         return acc / windZones.Count;
     }
 
-    
+    private float SeekEase(float origin, float target, float maxDelta, float outerThresh, float innerThresh)
+    {
+        float delta = target - origin;
+        float toMove = Mathf.Clamp(delta, -maxDelta, maxDelta);
+
+        if (Mathf.Abs(delta) > outerThresh) return origin + toMove;
+        if (Mathf.Abs(delta) > innerThresh) return origin + (toMove * (delta / outerThresh));
+        return target;
+    }
+
     #endregion
-    
+
 }
