1d15e5188c8650a627182a480cb5624723b99739
stevekrueger52@gmail.com <stevekrueger52@gmail.com>
2022-03-15 23:14:57 -0400

    I'm actually gonna get this shipped

--- Assets/_ShipOfThesis/Scripts/SailTarget.cs
+++ Assets/_ShipOfThesis/Scripts/SailTarget.cs
@@ -1,6 +1,16 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SailTarget : MonoBehaviour
 {
+    public delegate void CheckpointReachedHandler();
+    public event CheckpointReachedHandler CheckpointReached;
+
+    protected void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<Sailboat>() != null)
+        {
+            CheckpointReached?.Invoke();
+            Destroy(this);
+        }
+    }
 }
